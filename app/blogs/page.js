fa4c9a44be2851a2a286ebae168ef3eefd0d1ae1
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-08-08 23:07:04 +0200

    displaying blogs

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,8 +1,51 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from "react"
+import BlogList from "@/components/BlogList"
+
+// a component that is handling the blogs posting
+
+const BlogDisplay = ({ data }) => {
+  return (
+    <div className="mt-16">
+      {data.map((blog) => (
+        <BlogList
+          key={blog._id}
+          blog={blog}
+        />
+      ))}
+    </div>
+  )
+}
 
 const Blogs = () => {
+  const [searchFilter, setSearchFilter] = useState('')
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await fetch('/api/blog')
+      const data = await response.json()
+
+      setBlogs(data)
+    }
+
+    fetchBlogs()
+  }, [])
+
   return (
-    <div>Blogs</div>
+    <section
+      className="mt-16 mx-auto w-full max-w-xl flex justify-center items-center flex-col gap-2;"
+    >
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Search blog"
+          className="w-full border rounded-md px-6 py-3"
+        />
+      </form>
+      <BlogDisplay data={blogs} />
+    </section>
   )
 }
 
