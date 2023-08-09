057bdb5ebb27aea887e371090139d4e642678cf2
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-08-09 22:03:52 +0200

    fetching and displaying the blogs on the page

--- components/BlogList.js
+++ components/BlogList.js
@@ -1,11 +1,8 @@
 
 import Image from "next/image"
-import { useSession } from "next-auth/react"
-import { usePathname, useRouter } from "next/navigation"
 
 const BlogList = ({ blog }) => {
-    const { data: session } = useSession()
-    const router = useRouter()
+    console.log(blog.image)
     return (
         <section className="w-full">
             <div>
@@ -13,18 +10,18 @@ const BlogList = ({ blog }) => {
                     <div>
                         <Image
                             src={blog.author.image}
+                            alt="author"
                             width={40}
                             height={40}
                             className="rounded-full"
-                            alt="author"
                         />
                     </div>
-                    <Image
-                    src={`https://l3.unsplash.com/${blog.image}`}
+                    {/* <Image
+                    src={blog.image}
                     width={90}
                     height={100}
                     alt={blog.title}
-                     />
+                     /> */}
                     <h1>{blog.author.email}</h1>
                     <h2>{blog.title}</h2>
                 </div>
