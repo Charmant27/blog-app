4b9078b7f3971aade0cc3c6a9c6fd2aa573001a0
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-08-10 23:20:35 +0200

    added the basic data fetch

--- app/single-blog/[id]/page.js
+++ app/single-blog/[id]/page.js
@@ -1,10 +1,12 @@
 "use client"
+
 import { useEffect, useState } from "react"
 import { useSearchParams } from "next/navigation"
 
-const SingleBlog = () => {
+const singleBlog = () => {
   const searchParams = useSearchParams()
   const [blogId, setBlogId] = useState(null)
+  const [blog, setBlog] = useState({})
 
   useEffect(() => {
     const idParams = searchParams.get('id')
@@ -15,21 +17,31 @@ const SingleBlog = () => {
 
 
   useEffect(() => {
-    if (blogId !== null) {
-      const getBlog = async () => {
-        const response = await fetch(`/api/blog/${blogId}`);
-        const data = await response.json();
-        console.log(data);
-      };
-      getBlog();
-    }
+    console.log('fetching blog')
+    const getBlog = async () => {
+      const response = await fetch(`/api/blog/${blogId}`);
+      const data = await response.json();
+      console.log(response)
+      console.log(data);
+      setBlog(data)
+    };
+
+    getBlog();
   }, [blogId]);
 
   return (
     <div>
-      single blog
+      {Object.keys(blog).length > 0 ? (
+        <div>
+          <h1>{blog.title}</h1>
+          <p>Author: {blog.author.name}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+
     </div>
   )
 }
 
-export default SingleBlog
+export default singleBlog
